bd1ad931efd2686559260d9f1dfa970ce53a9f15
Kilian De Bock <Stualyttle@users.noreply.github.com>
2022-02-20 16:41:14 +0100

    22.7.7.2: changes to reset.css, and randomised placeholder of the input field, did styling input field.

--- Exercises/Just Do It!/public/scripts/main.js
+++ Exercises/Just Do It!/public/scripts/main.js
@@ -1,16 +1,43 @@
-// (() => {
-//   const app = {
-//     initialize() {
-//       this.cacheElements();
-//       this.generateUI();
-//     },
-//     cacheElements() {
-//       this. = document.querySelectorAll('selector');
-//       this. = document.querySelector('selector');
-//     },
-//     generateUI() {
-//     }
-//   };
-//   // Start initialization.
-//   app.initialize();
-// })();
+(() => {
+  const app = {
+    initialize() {
+      this.cacheElements();
+      this.generateUI();
+    },
+    cacheElements() {
+      // this.$ = document.querySelectorAll('selector');
+      this.$todoaddInput = document.querySelector('.todo-add input');
+    },
+    generateUI() {
+      const randomTodos = [
+          'Make Bed',
+          'Pick up clutter around the house',
+          'Clean what dishes are in the sink after breakfast',
+          'Spend 30 minutes in the garden or flowerbed',
+          'Walk the dog',
+          'Hit the gym',
+          'Pay bills',
+          'Meet George',
+          'Buy eggs',
+          'Make food',
+          'Read a book',
+          'Organize office',
+          'The Task You Will Forget',
+          'The Things You Absolutely Must Do Today',
+          'The New Habit',
+          'The Step Towards Your Goal',
+          'The Menial Task',
+          'The Promise You Made to Someone',
+          'The Task You Delegated',
+          'The Task You Need to Do For Yourself',
+          'The Task with a Deadline',
+          'The Answer You Are Waiting For'
+        ],
+        randomTodo = Math.floor(Math.random() * randomTodos.length);
+
+      this.$todoaddInput.placeholder = randomTodos[randomTodo];
+    }
+  };
+  // Start initialization.
+  app.initialize();
+})();
