1c776dddcd62d0b9bd0b1c5afe54725c1e2799f2
pgm-kilidebo <kilian.debock@student.arteveldehs.be>
2022-03-01 09:39:19 +0100

    22.9.1.2: Initial setup database.

--- Exercises/TypeORM Demo Start/src/app.js
+++ Exercises/TypeORM Demo Start/src/app.js
@@ -1,30 +1,47 @@
 import express from "express";
-import  "dotenv/config";
+import "dotenv/config";
 import * as path from "path";
 import { create } from "express-handlebars";
 import { SOURCE_PATH } from "./consts.js";
 import { home } from "./controllers/home.js";
 import HandlebarsHelpers from "./lib/HandlebarsHelpers.js";
+import bodyParser from "body-parser";
+import { createConnection } from "typeorm";
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static("public"));
+
+/**
+ * Body parser Init
+ */
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /**
  * Handlebars Init
  */
 const hbs = create({
   helpers: HandlebarsHelpers,
-  extname: "hbs"
+  extname: "hbs",
 });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-app.set("views", path.join(SOURCE_PATH, "views"))
+app.set("views", path.join(SOURCE_PATH, "views"));
 
 /**
  * App Routing
  */
-app.get('/', home);
+app.get("/", home);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Application is runninig on http://localhost:${process.env.PORT}/.`);
-})
+createConnection({
+  type: process.env.DATABASE_TYPE,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+}).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `Application is runninig on http://localhost:${process.env.PORT}/.`
+    );
+  });
+});
