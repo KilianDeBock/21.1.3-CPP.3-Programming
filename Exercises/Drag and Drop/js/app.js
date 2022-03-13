d70391b6838df21f4b3f9bc3249afff6533ba9c6
Kilian De Bock <Stualyttle@users.noreply.github.com>
2022-03-13 13:25:30 +0100

    22.10.7.1: Completed demo.

--- Exercises/Drag and Drop/js/app.js
+++ Exercises/Drag and Drop/js/app.js
@@ -19,17 +19,88 @@ const app = {
     this.initDragAndDrop();
   },
   cacheDOMElements() {
-    this.$stepsUl = document.querySelector("#steps");
-    this.$allStepsLi = document.querySelectorAll("#steps li");
+    this.$stepsUl = document.querySelector('#steps');
+    this.$allStepsLi = document.querySelectorAll('#steps li');
   },
   initOrder() {
     this.$allStepsLi.forEach(($stepLi) => {
       this.orderIDs.push($stepLi.id);
     });
   },
-  initDragAndDrop() {},
-  initReorder() {},
-  postNewOrder() {},
+  initDragAndDrop() {
+    let $currentLi = null;
+
+    this.$allStepsLi.forEach($stepLi => {
+      $stepLi.draggable = true;
+
+      // valid drop element maken
+      $stepLi.ondragover = (ev) => {
+        ev.preventDefault();
+      };
+
+      // Start dragging
+      $stepLi.ondragstart = (ev) => {
+        $currentLi = $stepLi;
+
+        this.$allStepsLi.forEach(i => i.classList.add('hint'));
+      };
+
+      // Stop dragging
+      $stepLi.ondragend = (ev) => {
+        $currentLi = $stepLi;
+
+        this.$allStepsLi.forEach(i => {
+          i.classList.remove('hint');
+          i.classList.remove('active');
+        });
+      };
+
+      $stepLi.ondragenter = (ev) => {
+        if ($stepLi === $currentLi) return false;
+        $stepLi.classList.add('active');
+      };
+
+      $stepLi.ondragleave = (ev) => {
+        $stepLi.classList.remove('active');
+      };
+
+      $stepLi.ondrop = (ev) => {
+        if ($stepLi === $currentLi) return false;
+
+        // Huidige idn wissen uit de array van id's
+        const currentInd = this.orderIDs.indexOf($currentLi.id);
+        this.orderIDs.splice(currentInd, 1);
+
+        // get index of dropped item (in orderids)
+        const newInd = this.orderIDs.indexOf($stepLi.id);
+
+        if (currentInd <= newInd) {
+          this.orderIDs.splice(newInd + 1, 0, $currentLi.id);
+        } else {
+          this.orderIDs.splice(newInd, 0, $currentLi.id);
+        }
+
+        this.initReorder();
+        this.postNewOrder();
+      };
+    });
+  },
+  initReorder() {
+    const newSteps = [];
+    this.orderIDs.forEach(id => {
+      const el = document.querySelector(`#${id}`);
+      newSteps.push(el);
+    });
+
+    this.$stepsUl.innerHTML = '';
+    newSteps.forEach(el => {
+      this.$stepsUl.appendChild(el);
+    });
+  },
+  postNewOrder() {
+    // fetch a post request, with new order
+    // ->> new oderIds
+  },
 };
 
 app.init();
--- Exercises/Drag and Drop/js/splash.js
+++ Exercises/Drag and Drop/js/splash.js
@@ -4,18 +4,58 @@ const splash = {
     this.initDragAndDrop();
   },
   cacheDOMElements() {
-    this.$pokeball = document.querySelector("#pokeball");
-    this.$pikachu = document.querySelector("#pikachu");
-    this.$splash = document.querySelector("#splash");
-    this.$order = document.querySelector("#order");
+    this.$pokeball = document.querySelector('#pokeball');
+    this.$pikachu = document.querySelector('#pikachu');
+    this.$splash = document.querySelector('#splash');
+    this.$order = document.querySelector('#order');
   },
   initDragAndDrop() {
     const dragEl = this.$pokeball;
     const dropEl = this.$pikachu;
+
+    dragEl.draggable = true;
+    // Word gestart met slepen
+    dragEl.ondragstart = (ev) => {
+      console.log('start');
+      dropEl.classList.add('jump');
+      ev.dataTransfer.effectAllowed = 'link';
+      ev.dataTransfer.setData('text/plain', 'Gotta catch \'em all!');
+    };
+    // Slepen word gestopt
+    dragEl.ondragend = (ev) => {
+      console.log('end');
+      dropEl.classList.remove('jump');
+    };
+    // We komen een drop area binnen
+    dropEl.ondragenter = (ev) => {
+      console.log('enter');
+      dropEl.classList.add('catch');
+
+    };
+    // We verlaten de drop area
+    dropEl.ondragleave = (ev) => {
+      console.log('hihi');
+      dropEl.classList.remove('catch');
+    };
+    // we slepen over het drop element
+    dropEl.ondragover = (ev) => {
+      ev.preventDefault();
+      console.log('ohn :\'(');
+    };
+    // we slepen over het drop element
+    dropEl.ondrop = (ev) => {
+      dropEl.classList.remove('jump');
+      dropEl.classList.remove('catch');
+      dropEl.classList.add('captured');
+      console.log('Ohhh <3');
+
+      // alert(ev.dataTransfer.getData('text/plain'));
+      setTimeout(() => this.exitGame(), 1000);
+    };
   },
   exitGame() {
-    this.$splash.classList.add("hidden");
-    this.$order.classList.remove("hidden");
+    this.$splash.classList.add('hidden');
+    this.$order.classList.remove('hidden');
   },
 };
 
