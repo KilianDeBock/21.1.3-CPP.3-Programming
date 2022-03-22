d308b48c44bde4e9d4e4da5814daa554b6fc0466
pgm-kilidebo <kilian.debock@student.arteveldehs.be>
2022-03-22 11:50:30 +0100

    22.12.2.4: Forgot to push the record.

--- Exercises/Seeding Demo/src/database/factories/AnimalFactory.js
+++ Exercises/Seeding Demo/src/database/factories/AnimalFactory.js
@@ -1,9 +1,8 @@
 import Factory from './Factory.js';
-import typeorm from 'typeorm';
 import TypeFactory from './TypeFactory.js';
-import animal from '@fakerjs/animal';
 import { faker } from '@faker-js/faker';
-
+import animal from '@fakerjs/animal';
+import typeorm from 'typeorm';
 
 const { createConnection, getConnection } = typeorm;
 
@@ -13,37 +12,35 @@ class AnimalFactory extends Factory {
     this.types = TypeFactory.types;
   }
 
-  // Make one record
+  // make one record
   async make() {
-    // Make them all
     const randInd = Math.floor(Math.random() * this.types.length);
     const randType = this.types[randInd];
 
     const randAnimal = animal(randType);
-    console.log(randAnimal, randType);
 
     const beast = {
       name: randAnimal,
-      color: faker.commerce.color,
+      color: faker.commerce.color(),
       latinName: faker.lorem.word(),
       isFluffy: faker.datatype.boolean(),
     };
 
-    console.log(beast);
-
     const record = await this.insert(beast, randType);
+
+    this.inserted.push(record);
   }
 
   async insert(animal, type) {
     const repo = getConnection().getRepository('Animal');
 
-    // Record exists?
+    // record exists?
     let record = await repo.findOne({ where: { name: animal.name } });
     if (record) return record;
 
     const typeRecord = await TypeFactory.insert(type);
 
-    // Create record
+    // create record
     record = await repo.save({
       name: animal.name,
       color: animal.color,
@@ -54,9 +51,7 @@ class AnimalFactory extends Factory {
       },
     });
 
-    console.log(record);
-
-    // Return
+    // return
     return record;
   }
 }
